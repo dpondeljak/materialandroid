daefe362134724bb50c9621048411061f7bc59f3
Andrew Lord <andrew@andrew-lord.co.uk>
2016-03-16 19:52:41 +0000

    Added support library to travis build script. Used some material resources in sample. Started work on sample.

--- sample/src/main/java/com/github/andrewlord1990/materialandroidsample/SampleActivity.java
+++ sample/src/main/java/com/github/andrewlord1990/materialandroidsample/SampleActivity.java
@@ -13,49 +13,90 @@
 package com.github.andrewlord1990.materialandroidsample;
 
 import android.os.Bundle;
+import android.support.annotation.ColorRes;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class SampleActivity extends AppCompatActivity {
+
+    private View primaryColorSquare;
+    private View accentColorSquare;
+    private Button colorSample;
+
+    @ColorRes
+    private static int defaultPrimaryColor = R.color.md_cyan_500;
+
+    @ColorRes
+    private static int defaultAccentColor = R.color.md_red_a200;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_sample);
+
+        setupToolbar();
+        setupFab();
+
+        setupColorSample();
+    }
+
+    private void setupFab() {
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        if (fab != null) {
+            fab.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                            .setAction("Action", null).show();
+                }
+            });
+        }
+    }
+
+    private void setupToolbar() {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+    }
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+    private void setupColorSample() {
+        primaryColorSquare = findViewById(R.id.primary_color_square);
+        if (primaryColorSquare != null) {
+            primaryColorSquare.setBackgroundColor(ContextCompat.getColor(this, defaultPrimaryColor));
+            primaryColorSquare.setOnClickListener(new OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                }
+            });
+        }
+        accentColorSquare = findViewById(R.id.accent_color_square);
+        accentColorSquare.setBackgroundColor(ContextCompat.getColor(this, defaultAccentColor));
+        accentColorSquare.setOnClickListener(new OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+
             }
         });
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_sample, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        colorSample = (Button) findViewById(R.id.color_sample_button);
+        if (colorSample != null) {
+            colorSample.setOnClickListener(new OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    onColorSampleSelected();
+                }
+            });
         }
-
-        return super.onOptionsItemSelected(item);
     }
+
+    private void onColorSampleSelected() {
+
+    }
+
 }
